b50db9aa464df3c84ea4e5a11f30d80c10eac445
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-03 16:24:15 +0000

    made HTML and JS connect and function run based off input values

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const input = document.getElementById('numbersInput');
+
+const output = document.getElementById('output');
+
+const btn = document.getElementById('submitBtn');
+
+btn.addEventListener('click', evenNumbersOnly);
 
 
-function evenNumbersOnly(numbers) {
- let evenNumbers = numbers.filter(number % 2 === 0);
-  return evenNumbers;
+function evenNumbersOnly() {
+
+  //grab the value
+  let inputValue = input.value;
+
+  //ready the data
+  const numbers = inputValue.split(',').map(num => parseInt(num.trim())).filter(num => !isNaN(num));
+
+  //filter the data
+  const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+
+  output.innerText = `[${evenNumbers}]`;
+
 }
